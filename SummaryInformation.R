8bd41f4de44b20f5889ed660d4a7e106bc6f2b3a
Enoch Lee <treezich@gmail.com>
2021-02-16 12:14:21 -0600

    add summary and data

---
+++ SummaryInformation.R
@@ -0,0 +1,33 @@
+# This file contains analysis on the datasets themselves. 
+# One example would be numbers of rows/columns in the dataset.
+
+movies <- read.csv(paste0("C:\\Users\\treez\\OneDrive\\Documents\\info201\\",
+                   "project-architasingh\\Data\\",
+                   "MoviesOnStreamingPlatforms_updated.csv"),
+                   stringsAsFactors = FALSE)
+
+# Create a list to store basic information like number of rows and 
+# features, then see what those features are.
+summary_info <- list()
+summary_info$num_observations <- nrow(movies)
+summary_info$num_features <- ncol(movies)
+summary_info$feature_names <- colnames(movies)
+
+# Investigating the highest rating for IMDb and Rotten Tomates, then
+# counting the number of these high ratings.
+
+summary_info$IMDb_highest_rating <- movies %>%
+  filter(IMDb == max(IMDb, na.rm = TRUE)) %>%
+  pull(IMDb)
+
+summary_info$num_IMDb_highest_rating <- movies %>%
+  filter(IMDb == max(IMDb, na.rm = TRUE)) %>%
+  count()
+
+summary_info$RT_highest_rating <- movies %>%
+  filter(Rotten.Tomatoes == max(Rotten.Tomatoes)) %>%
+  pull(Rotten.Tomatoes)
+
+summary_info$num_RT_highest_rating <- movies %>%
+  filter(Rotten.Tomatoes == max(Rotten.Tomatoes)) %>%
+  count()
