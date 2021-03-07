e3444c2662ecf5c086d777b4c67d81d6216837fb
architasingh <49347272+architasingh@users.noreply.github.com>
2021-03-07 14:52:17 -0800

    fixed pie chart legend

--- pie_chart.R
+++ pie_chart.R
@@ -32,27 +32,38 @@ pie_chart <- function() {
   
   #create table with age groups, movie amount in each age group, and 
   #proportion of each age group
-  age_group_amt.data <- data.frame(Age_Group = c("7+", "13+", "16+", "18+", "all"),
-                                   Amount = c(num_seven, num_thirteen, num_sixteen, num_eighteen, num_all),
-                                   prop = c(round(num_seven/total * 100, 2), round(num_thirteen/total* 100, 2), round(num_sixteen/total * 100, 2), round(num_eighteen/total * 100, 2), round(num_all/total * 100, 2)))
-  age_group_amt.data
+  age_group_amt.data <- data.frame(Age_Group = c("7+", "13+", "16+", "18+", 
+                                                 "all"),
+                                   Amount = c(num_seven, num_thirteen, 
+                                              num_sixteen, num_eighteen, num_all),
+                                   prop = c(round(num_seven/total * 100, 2), 
+                                            round(num_thirteen/total* 100, 2), 
+                                            round(num_sixteen/total * 100, 2), 
+                                            round(num_eighteen/total * 100, 2), 
+                                            round(num_all/total * 100, 2)))
   
   #add label position
-  age_group_amt.data <- age_group_amt.data %>%
-    arrange(desc(Age_Group)) %>%
-    mutate(lab.ypos = round(cumsum(prop) - 0.5*prop, 2))
-  age_group_amt.data
   
-  mycols <- c("#031A6B", "#004385", "#033860", "#087CA7", "#05B2DC")
+  mycols <- c("#CD5334", "#073B4C", "#88527F", "#BA1200", "#379392")
+  
+  age_group_amt.data$Age_Group <- factor(age_group_amt.data$Age_Group, levels=c("7+", "13+", "16+", 
+                                                            "18+", "all"), 
+                               labels=c("7+", "13+", "16+", "18+", "all"))
+  
+  age_group_amt.data <- age_group_amt.data %>%
+    arrange(desc(Age_Group)) %>% 
+    mutate(lab.ypos = round(cumsum(prop) - 0.5*prop, 2))
   
   #create pie chart
-  pie_chartt <- ggplot(age_group_amt.data, aes(x = "", y = prop, fill = Age_Group)) +
+  pie_chartt <- ggplot(age_group_amt.data, aes(x = "", y = prop, 
+                                               fill = Age_Group)) +
     geom_bar(width = 1, stat = "identity", color = "white") +
-    coord_polar("y", start = 0)+
+    coord_polar("y", start = 0) +
     geom_text(aes(y = lab.ypos, label = prop), color = "white") +
     scale_fill_manual(values = mycols) +
     labs (title = "Movie Distribution Across Different Age Groups") +
     labs(fill = 'Age Groups') +
-    theme_void()
+    theme_void() 
+    
   return(pie_chartt)
 }
